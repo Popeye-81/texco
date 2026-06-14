45e77a1311bd015c8f54fbbd34a29ceea3770fc2
Soumya Kurel <soumya.kurel@gmail.com>
2026-06-14 16:40:43 +0530

    add product master api

---
+++ api/auth/users.js
@@ -0,0 +1,7 @@
+export const users = [
+  { username: "cso1", password: "123", role: "CSO" },
+  { username: "ase1", password: "123", role: "ASE" },
+  { username: "sm1", password: "123", role: "SM" },
+  { username: "asm1", password: "123", role: "ASM" },
+  { username: "admin", password: "admin123", role: "ADMIN" },
+];
